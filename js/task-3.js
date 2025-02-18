ed6f02856ca8d5f95fe73d502c8dc59c297959a2
tymur_lykho <tima.likho@gmail.com>
2025-02-18 10:43:42 +0200

    Fix bug in 3 task

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,7 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-input.addEventListener('input', () => { 
-	if (input.value !== '') {
-		output.textContent = input.value.trim();
-	} else {
-		output.textContent = 'Anonymous';
-	}
-})
+input.addEventListener('input', () => {
+  const inpVal = input.value.trim();
+  output.textContent = inpVal !== '' ? inpVal : 'Anonymous';
+});
